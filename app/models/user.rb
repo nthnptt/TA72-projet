06ca462207c95ca045264120f5b2fc0cc358ddef
yaman0 <yaman0@protonmail.com>
2019-10-21 21:35:15 +0200

    fix(): fix user create without images

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
   has_one_attached :avatar
   validates_length_of :phone_number, :is => 10
   validates_presence_of :username, :phone_number, :city, :city_code
+  before_validation :default_img
+
+  def default_img
+    if self.avatar.blank?
+      self.avatar.attach(io: File.open('app/assets/images/default_profil.png'),
+                     filename: 'placeholder.png', content_type: 'image/png')
+    end
+
+  end
+
+
 end
