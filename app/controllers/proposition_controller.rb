7f9568838664a7b6228d216fc0613db1972b5eb6
yaman0 <yaman0@protonmail.com>
2019-10-26 11:22:03 +0200

    :hammer: user can't create 2 propositions for the same offer

--- app/controllers/proposition_controller.rb
+++ app/controllers/proposition_controller.rb
@@ -12,19 +12,14 @@ class PropositionController < ApplicationController
                     })
     proposition.offer = offer
     proposition.user = current_user
-    if proposition.save
-      redirect_to offer_proposition_path(offer, proposition)
-    elsif proposition.errors.details[:user_id].include?({error: :taken, value: current_user.id})
-      flash[:notice] = "Proposition already exist, you've been redirect to it"
-      proposition = Proposition.find_by(user_id: current_user, offer_id: offer.id)
-      redirect_to offer_proposition_url(offer, proposition)
-    else
-      flash[:alert] = proposition.errors.messages
-      @propositions = offer.propositions
-      render 'index'
-    end
-
+    proposition.save!
+    redirect_to offer_proposition_path(offer, proposition)
+  rescue ActiveRecord::RecordInvalid => error
+    puts error.inspect
+    redirect_to offer_proposition_index_path, notice: error.inspect
   end
+
+#findorcreate
 
   def show
     @proposition = Proposition.find params[:id]
