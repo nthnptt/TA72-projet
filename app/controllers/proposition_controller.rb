64c7a82c77ea3e36fc7d8d5ac21ee087df0bd632
yaman0 <yaman0@protonmail.com>
2019-10-26 09:59:33 +0200

    :sparkles: user can't create 2 propositions for the same offer

--- app/controllers/proposition_controller.rb
+++ app/controllers/proposition_controller.rb
@@ -12,8 +12,18 @@ class PropositionController < ApplicationController
                     })
     proposition.offer = offer
     proposition.user = current_user
-    proposition.save
-    redirect_to offer_proposition_path(offer, proposition)
+    if proposition.save
+      redirect_to offer_proposition_path(offer, proposition)
+    elsif proposition.errors.details[:user_id].include?({error: :taken, value: current_user.id})
+      flash[:notice] = "Proposition already exist, you've been redirect to it"
+      proposition = Proposition.find_by(user_id: current_user, offer_id: offer.id)
+      redirect_to offer_proposition_url(offer, proposition)
+    else
+      flash[:alert] = proposition.errors.messages
+      @propositions = offer.propositions
+      render 'index'
+    end
+
   end
 
   def show
--- app/models/proposition.rb
+++ app/models/proposition.rb
@@ -1,4 +1,5 @@
 class Proposition < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+  validates_uniqueness_of :user_id, scope: :offer_id
 end
