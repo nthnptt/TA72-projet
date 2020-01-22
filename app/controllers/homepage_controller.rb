4081fe49fe296d3a21ce48b21b8343747e06f0b7
yaman0 <yaman0@protonmail.com>
2020-01-22 14:39:33 +0100

    :feat: avoid homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class HomepageController < ApplicationController
-  def index; end
+  def index
+    - if user_signed_in?
+        redirect_to '/offers'
+      else
+        redirect_to '/users/sign_in'
+      end
+
+  end
 end
